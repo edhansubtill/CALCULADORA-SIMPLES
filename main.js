751e08ecfff923592fc77e0bd799b7a0ae268f1c
edhansubtill <141247816+edhansubtill@users.noreply.github.com>
2023-10-18 11:21:10 -0300

    CalculadoradoEdhan

---
+++ main.js
@@ -0,0 +1,23 @@
+// main.js
+
+let display =document.getElementById ('display');
+let currentlnput = "";
+
+function appendToDisplay(value) {
+    currentlnput += value;
+    display.value = currentlnput;
+}
+
+function clearDisplay() {
+    currentlnput = " ";
+    display.value = " ";
+}
+
+function clearDisplay() {
+     try {
+         currentlnput = eval(currentlnput);
+         display.value = currentlnput;
+     } catch (error) {
+         display.value = "Erro":
+     }
+}
